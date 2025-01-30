50cd2e112de79fdbe629735b7e912ea12add2cdf
yaquitadeveloper <yaquitadeveloper@gmail.com>
2025-01-29 20:07:23 -0500

    modificacion de lineas de codigo

--- js/guardarcat.js
+++ js/guardarcat.js
@@ -5,7 +5,7 @@ function guardarCategoria(event) {
     const nombre = categoriaElement.querySelector('span').textContent;
     sessionStorage.setItem('categoriaImagen', imagen);
     sessionStorage.setItem('categoriaNombre', nombre);
-    setTimeout(function() {
+    setTimeout(function () {
         window.location.href = "trivia.html";
     }, 1);
 }
@@ -19,4 +19,25 @@ function cargarCategoria() {
         console.error('No se encontraron datos en sessionStorage');
         document.getElementById('categoriaNombre').textContent = "Selecciona tu categoría 😁";
     }
+}
+
+
+function iniciarJuego() {
+    // Obtener valores seleccionados
+    const dificultad = document.getElementById("selectordificultad").value;
+    const tiempo = document.getElementById("selectortiempo").value;
+    const categoria = sessionStorage.getItem("categoriaNombre");
+
+    // Validar
+    if (!dificultad || !tiempo || !categoria) {
+        alert("¡Completa todas las opciones! 😊");
+        return;
+    }
+
+    // Guardar en sessionStorage
+    sessionStorage.setItem("dificultad", dificultad);
+    sessionStorage.setItem("tiempo", tiempo);
+
+    // Redirigir a contenido.html
+    window.location.href = "contenido.html";
 }
